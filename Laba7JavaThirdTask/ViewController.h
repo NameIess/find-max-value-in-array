1b6b58422d6ca20c85da5c9f8b09e66e1b9b6de9
Dima <dima@Dimas-Mac.local>
2018-11-01 20:52:20 +0300

    Initial Commit

---
+++ Laba7JavaThirdTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Laba7JavaThirdTask
+//
+//  Created by Dima  on 11/1/18.
+//  Copyright © 2018 Dima . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
